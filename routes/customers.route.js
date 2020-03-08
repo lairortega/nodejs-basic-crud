a94d8799937ec510ee88871f12df6dd82e5f4a80
Lair Ortega Dehonor <lair.ortega@gmail.com>
2020-03-08 13:57:29 -0600

    middleware rutas de clientes

---
+++ routes/customers.route.js
@@ -0,0 +1,45 @@
+var handler = function(router){
+    router.get('/', function(req, res, next){
+        res.json({
+            method: "GET",
+            path: "/customers",
+            comment: "Method reseved to list all customers."
+        });
+    });
+
+    router.post('/', function(req, res, next){
+        res.json({
+            method: "POST",
+            path: "/customers",
+            comment: "Method design to create customer."
+        });
+    });
+
+    router.put('/', function(req, res, next){
+        res.json({
+            method: "PUT",
+            path: "/customers",
+            comment: "Update customer."
+        });
+    });
+
+    router.patch('/', function(req, res, next){
+        res.json({
+            method: "PATCH",
+            path: "/customers",
+            comment: "Modify customer."
+        });
+    });
+
+    router.delete('/', function(req, res, next){
+        res.json({
+            method: "DELETE",
+            path: "/customers",
+            comment: "Erase customer."
+        });
+    });
+
+    return router;
+}
+
+module.exports = handler;
